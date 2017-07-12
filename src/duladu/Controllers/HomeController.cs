b796397e6a78a978866f18e5f2dcaffb5edfbbd7
Midori <midori.fukami@hotmail.com>
2017-07-11 22:52:21 -0300

    add project
    desktop layout and email notification done

---
+++ src/duladu/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace duladu.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Seller")]
+        public IActionResult Seller()
+        {
+            ViewData["Message"] = "Seller";
+
+            return View();
+        }
+
+        [Route("Buyer")]
+        public IActionResult Buyer()
+        {
+            ViewData["Message"] = "Buyer";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/duladu/wwwroot/js/seller.js
@@ -0,0 +1,35 @@
+﻿function submit() {
+    var email = $('#email').val();
+    if (validateEmail(email)) {
+        $('.validate').hide();
+        $('.email-valid').css('padding-bottom', '20px');
+        // send email
+        console.log(email);
+        
+        $.confirm({
+            backgroundDismiss: function () {
+                $('#email').val('');
+                return true; // modal close.
+            },
+            title: 'E-mail cadastrado com sucesso!',
+            content: 'Entraremos em contato no lançamento :)',
+            buttons: {
+                OK: {
+                    btnClass: 'btn-default',
+                    action: function () {
+                        $('#email').val('');
+                    }
+                }
+            }
+        });
+    }
+    else {
+        $('.email-valid').css('padding-bottom', '0');
+        $('.validate').show();
+    }
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
